1cc4993518f9df6524a0f59b8aa1650482652f39
Dave Mayo <dave_mayo@harvard.edu>
2015-06-25 15:52:55 -0400

    Included mimetype for proxy methods.

--- proxy/views.py
+++ proxy/views.py
@@ -8,19 +8,19 @@ from logging import getLogger
 logger = getLogger(__name__)
 
 WEB_SERVICE_URL = environ['PDS_WS_URL']
-methods_allowed = ["printpdf",
-                   "find",
-                   "get",
-                   "related",
-                   "cite"]
+methods = {"printpdf": "application/pdf",
+           "find": "application/xml",
+           "get": "application/xml",
+           "related": "application/xml",
+           "cite": "application/json"}
 
 
 # view for proxy url
 def proxy(request, method, record_id):
-    if not method in methods_allowed:
+    if not method in methods_allowed.keys():
         return HttpResponseForbidden("Method not allowed by proxy")
 
     if 'hulaccess' in request.COOKIES:
         ams_cookie = request.COOKIES['hulaccess']
 
-    return HttpResponse(webclient.get(WEB_SERVICE_URL + method + "/" + record_id + "?" + request.GET.urlencode(), ams_cookie))
+    return HttpResponse(webclient.get(WEB_SERVICE_URL + method + "/" + record_id + "?" + request.GET.urlencode(), ams_cookie), content_type=methods[method])
