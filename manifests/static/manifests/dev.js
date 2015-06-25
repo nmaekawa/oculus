11e0daee322ae762050fb4371cf378d72535bce6
Chip Goines <chip_goines@harvard.edu>
2015-06-25 02:32:41 -0400

    jqlistbox removed callback for search

--- manifests/static/manifests/dev.js
+++ manifests/static/manifests/dev.js
@@ -231,8 +231,9 @@ $(function() {
           height: 200,
           renderer: function (index, label, value) {
             //style="margin: 5px; float:left;word-wrap: break-word; height: auto; overflow-x: auto;"
-            var record = this.records[index];
-            console.log(this.records);
+            var record = dataAdapter.records[index];
+            console.log(dataAdapter.records);
+            console.log(record);
             var cell = "<i>" + label + "</i><br>" + record.context;
             return cell;
           }
