098329c546136154df6e7e85d754906181001199
Dave Mayo <dave_mayo@harvard.edu>
2015-06-26 16:08:38 -0400

    Attempt at implementing single-quote url separator

--- manifests/static/manifests/dev.js
+++ manifests/static/manifests/dev.js
@@ -157,7 +157,7 @@ $(function() {
           return 'drs:' + drs_id + '$' + n + ftype_alias[focusType];
         }
         else {
-          return uri.replace(/^https?:\/\//, 'ext:') + '$' + n + ftype_alias[focusType];
+          return uri.replace(/^https?:\/\//, 'ext:').replace(/\//g, "'") + '$' + n + ftype_alias[focusType];
         }
       }
     });
