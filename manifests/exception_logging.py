24e53f7a132c6ca60b7c313676116e71d15d9157
Dave Mayo <dave_mayo@harvard.edu>
2015-07-02 12:27:34 -0400

    Print string-rep of exception, too

--- manifests/exception_logging.py
+++ manifests/exception_logging.py
@@ -3,4 +3,5 @@ logger = logging.getLogger(__name__)
 
 class ExceptionLoggingMiddleware(object):
     def process_exception(self, request, exception):
-        logger.debug('Exception handling request for ' + request.path)
+        logger.debug('Exception handling request for ' + request.path + ':')
+        logger.debug("\t" + str(exception))
